88e14b100cb6d2ce06effdd8a4db27d0d932a2fc
evadb <evademersbrett@gmail.com>
2018-07-24 15:42:54 -0400

    Comitting before trying anything crazy

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    webpack: (config, { dev }) => {
-      config.module.rules.push(
-        {
-          test: /\.css/,
-          loader: 'emit-file-loader',
-          options: {
-            name: 'dist/[path][name].[ext]'
-          }
-        },
-        {
-          test: /\.css$/,
-          use: ['babel-loader', 'raw-loader', 'postcss-loader']
+  webpack: (config, { dev }) => {
+    config.module.rules.push(
+      {
+        test: /\.css/,
+        loader: 'emit-file-loader',
+        options: {
+          name: 'dist/[path][name].[ext]'
         }
-      )
-      return config;
-    }
+      },
+      {
+        test: /\.css$/,
+        use: ['babel-loader', 'raw-loader', 'postcss-loader']
+      }
+    )
+    return config;
   }
+}
